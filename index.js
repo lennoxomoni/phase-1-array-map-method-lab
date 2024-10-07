03217e9f748aef469fbd03ba4b37ffe4ca1dcc4d
lennox <lennoxngubo549@gmail.com>
2024-10-07 15:39:07 +0300

    phase-1-array-map-method-lab

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((tutorial) => {
+    let tutorialArray =tutorial.split(" ");
+    return tutorialArray.map((word) =>{
+      return word[0].toUpperCase() + word.slice(1)
+    }).join(" ")
+  })
 }
+//console.log(titleCased)(tutorials)
